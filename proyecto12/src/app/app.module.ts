b836d71ccaf16566c27d35ca4de0bfd58795e0ba
Carlos Portillo <carlosportillo308@gmail.com>
2023-01-06 10:50:50 -0500

    segundo commit

--- proyecto12/src/app/app.module.ts
+++ proyecto12/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body.component';
+import { CondicionalDirective } from './condicional.directive';
+import { ComponentesComponent } from './componentes/componentes.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    CondicionalDirective,
+    ComponentesComponent,
+    FormularioComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ proyecto12/src/app/directivas/condicional.directive.spec.ts
@@ -0,0 +1,8 @@
+import { CondicionalDirective } from './condicional.directive';
+
+describe('CondicionalDirective', () => {
+  it('should create an instance', () => {
+    const directive = new CondicionalDirective();
+    expect(directive).toBeTruthy();
+  });
+});
