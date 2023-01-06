a6b506410c56cb1f9c3c971a81281cd41c966109
CFPH31415 <121558966+CFPH31415@users.noreply.github.com>
2023-01-06 10:58:22 -0500

    Update app.module.ts

--- proyecto12/src/app/app.module.ts
+++ proyecto12/src/app/app.module.ts
@@ -3,20 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BodyComponent } from './body/body.component';
-import { CondicionalDirective } from './condicional.directive';
-import { ComponentesComponent } from './componentes/componentes.component';
-import { FormularioComponent } from './formulario/formulario.component';
-import { HomeComponent } from './pages/home/home.component';
+import { CondicionalDirective } from './directivas/condicional.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BodyComponent,
-    CondicionalDirective,
-    ComponentesComponent,
-    FormularioComponent,
-    HomeComponent
+    CondicionalDirective
   ],
   imports: [
     BrowserModule,
